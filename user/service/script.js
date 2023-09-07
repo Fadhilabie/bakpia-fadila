fe18a50223ed4bd1be3ee069df67931b44444473
Fadhilabie <fadhilmuhhab@gmail.com>
2023-09-07 19:20:29 +0700

    first commit

---
+++ user/service/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiperPopuler", {
+  grabCursor: true,
+  centeredSlides: false,
+  spaceBetween: 20,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".fa-circle-chevron-right",
+    prevEl: ".fa-circle-chevron-left",
+  },
+});
